a33db91270b5f5afa2202af5c2d3e19bc9ce1ae3
answeror <answeror@gmail.com>
2012-07-24 21:08:10 +0800

    add doctest of settings

--- memoit/settings.py
+++ memoit/settings.py
@@ -17,6 +17,14 @@ def signal_name(key):
     return key + '_changed'
 
 
+def getter_name(key):
+    return key
+
+
+def setter_name(key):
+    return 'set_' + key
+
+
 def signal(type):
     """Object act like a pyqtSignal."""
     class Signal(QObject):
@@ -34,11 +42,41 @@ def signal(type):
 
 def eat(iterable):
     """Eat iterable elements."""
-    for e in iterable: pass
+    for e in iterable:
+        pass
 
 
-def settings(items, organization=None, application=None):
+def settings(items, *args, **kargs):
     """Construct a settings object.
+
+    ``args`` and ``kargs`` are used as ``QSettings`` parameters,
+    see `http://qt-project.org/doc/qt-4.8/qsettings.html`_ for details.
+
+    >>> from PyQt4.QtCore import QSettings
+    >>> from tempfile import NamedTemporaryFile as ntf
+    >>> f = ntf(suffix='.ini', delete=False)
+    >>> s = settings(dict(name='foo', age=42), f.name, QSettings.IniFormat)
+    >>> print(s.name, s.age)
+    foo 42
+    >>> s.name, s.age = 'bar', 13
+    >>> print(s.name, s.age)
+    bar 13
+
+    >>> from PyQt4.QtCore import QObject, pyqtSlot
+    >>> class Observer(QObject):
+    ...     @pyqtSlot(str)
+    ...     def react(self, value):
+    ...         print('new name ' + value)
+    >>> class Trigger(QObject):
+    ...     send = pyqtSignal(str)
+    >>> f = ntf(suffix='.ini', delete=False)
+    >>> s = settings(dict(name='foo'), f.name, QSettings.IniFormat)
+    >>> t = Trigger()
+    >>> t.send.connect(s.set_name)
+    >>> o = Observer()
+    >>> s.name_changed.connect(o.react)
+    >>> t.send.emit('bar')
+    new name bar
 
     :param items: keys and their default values
     :type items: dict of (str, any type)
@@ -48,14 +86,13 @@ def settings(items, organization=None, application=None):
 
         def __init__(self):
             super(Settings, self).__init__()
-            if not organization is None and not application is None:
-                self.settings = QSettings(organization, application)
-            else:
-                self.settings = QSettings()
+            self.settings = QSettings(*args, **kargs)
 
     def deal(key, default):
         def get(self):
             return self.settings.value(key, default)
+
+        get.__name__ = getter_name(key)
 
         def set(self, value):
             if getattr(self, key) == value:
@@ -63,9 +100,13 @@ def settings(items, organization=None, application=None):
             self.settings.setValue(key, value)
             getattr(self, signal_name(key)).emit(value)
 
+        set.__name__ = setter_name(key)
+
         setattr(Settings, key, pyqtProperty(type(default), get, set))
         # cannot directly use pyqtSignal here, cause python crash
         setattr(Settings, signal_name(key), signal(type(default)))
+        setattr(Settings, setter_name(key), set)
+        setattr(Settings, setter_name(key), getattr(Settings, setter_name(key)))
 
     eat(starmap(deal, items.items()))
 
